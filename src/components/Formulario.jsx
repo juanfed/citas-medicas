b01b07ae8c5dd1ee15b7c7d96256733aab70a088
juanfed <juanfed12345@gmail.com>
2022-02-22 15:10:29 -0500

    ya puedo leer los datos que usuario ingrese en los inputs

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,48 +1,67 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../styles/formulario.css';
 const Formulario = () => {
 
     //creacion de las citas
     const [citas, setcitas] = useState({
-        nombre: "",
-        apellido: "",
-        fecha: "",
-        sintomas: ""
+        nombre: '',
+        apellido: '',
+        fecha: '',
+        numero : '',
+        sintomas: ''
     });
 
     // funcion que se ejecuta cuando el usuario escribe en los inputs
-    const actualizar = () =>{
-        console.log("El usuario esta escribiendo")
+    const actualizar = (e) =>{  // le paso un evento, sirve para guardar la informacion, si usamos e.target.nameme indicara el nombre del campo en el que escribo
+        setcitas({
+            ...citas, // hago copia de la cita y le añado el valor que estoy escribiendo en el input cada vez que se hace se interactua
+            [e.target.name] : e.target.value
+        })
     }
+
+    // estraigo los valores
+
+    const {nombre, apellido, fecha, numero, sintomas } = citas;
+
     return (
         <div>
             <h2>Formulario cita medica</h2>
             <form className='formulario'>
                 <label htmlFor="name">Nombre </label>
                 <input type="text" 
+                name='nombre'
                 id='name' 
+                value={nombre}  // nos permitira resetear los valores mas adelante
                 placeholder='Ingrese el nombre'
                 onChange={actualizar} />
 
                 <label htmlFor="lastName">Apellido </label>
                 <input type="text"
+                name='apellido'
                 id='lastName'
+                value={apellido}
                 placeholder='Ingrese el apellido'
                 onChange={actualizar} />
 
                 <label htmlFor='date'>Fecha </label>
                 <input type="date" 
+                name='fecha'
                 id='date'
+                value={fecha}
                 onChange={actualizar} />
 
                 <label htmlFor="number">Número </label>
                 <input type="number"
+                name='numero'
                 id='number'
+                value={numero}
                 placeholder='Número celular' 
                 onChange={actualizar}/>
 
                 <label htmlFor="sintomas">Sintomas</label>
                 <textarea id="sintomas" cols="30" rows="5"
+                name='sintomas'
+                value={sintomas}
                 onChange={actualizar}></textarea>
                 
             </form>
